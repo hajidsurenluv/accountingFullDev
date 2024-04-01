fe0bdf0ff8b8f5c837c509da65ff54e3f4d21363
enkhboldzu <enkhboldzu@gmail.com>
2024-04-01 10:12:52 +0800

    calculator test

---
+++ calculator/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Calculator />
+</template>
+
+<script>
+import Calculator from "./components/Calculator.vue";
+export default {
+  name: "App",
+  components: {
+    Calculator,
+  },
+};
+</script>
