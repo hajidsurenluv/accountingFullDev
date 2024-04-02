c6e3897c65d373ee93406f34b0ec8c301ed66db1
enkhboldzu <enkhboldzu@gmail.com>
2024-04-02 13:34:04 +0800

    2

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+var path = require("path");
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening on port ${port}`)
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="py-4">
+      <h1 class="font-bold text-2xl text-white md:text-4xl" v-html="text"></h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HeaderComp",
+    props: ["text"],
+  };
+  </script>
